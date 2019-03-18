8e654880d559bdb6b77fbc1dd5854f75913a4fd1
Hannes Bornman <Hannes@explore-datascience.net>
2019-03-18 11:29:29 +0200

    added recursion.py

---
+++ mypackage/recursion.py
@@ -0,0 +1,20 @@
+def sum_array(array):
+    '''Return sum of all items in array'''
+    return sum(array)
+
+def fibonacci(n):
+    '''return nth term in fibonacci sequence'''
+    if n==1 or n==2:
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
+
+def factorial(n):
+    '''return n!'''
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+def revers(word):
+    '''return word in reverse'''
+    return word[::-1]
