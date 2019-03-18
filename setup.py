1357ad6655690a61bfb9fe20389b7bc22247f8c2
Hannes Bornman <Hannes@explore-datascience.net>
2019-03-18 11:53:52 +0200

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mypackage',
-    version='0.2',
+    version='0.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
